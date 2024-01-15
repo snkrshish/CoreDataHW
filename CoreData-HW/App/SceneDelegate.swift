4cd43da15d283d9c9a926d68a4c8e7769ae65f09
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2024-01-16 07:50:23 +0800

    start setting

---
+++ CoreData-HW/App/SceneDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  SceneDelegate.swift
+//  CoreData-HW
+//
+//  Created by Никита Шиляев on 16.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = MainViewController()
+        window.rootViewController = UINavigationController(rootViewController: viewController)
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
---
+++ CoreData-HW/Sources/Views/MainViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
